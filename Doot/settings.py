0ef4bba936cba060c9955b5b270f2d290c6491bf
TrainingLaptop <adaromosu@gmail.com>
2023-09-13 08:20:57 +0100

    upgrading soon

--- Doot/settings.py
+++ Doot/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3_ugx(%e9z=jf6ydt+n^i#=9s^t&23&vgl7wf6zrv6sta8v62o'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
